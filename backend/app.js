63d6274183eb043f569b9023803ab61cd6b6e7fb
Nikita Sukhov <veyron-nike@yandex.ru>
2023-07-09 17:55:34 +0300

    back change port on 3000

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
